bbdcbfd7a59efdf820038c4f1bb19aa7cddcce18
kylieainslie <ainslie.kylie@gmail.com>
2022-11-09 15:31:11 +0100

    - remove extra recovered states - change alpha from rate to number of people vaccinated per day - allow people who have recovered to be vaccinated

--- R/age_struct_seir_simple.R
+++ R/age_struct_seir_simple.R
@@ -34,47 +34,42 @@ age_struct_seir_simple <- function(times, init, params) {
     R <- c(R1, R2, R3, R4, R5, R6, R7, R8, R9)
     Rv <- c(Rv1, Rv2, Rv3, Rv4, Rv5, Rv6, Rv7, Rv8, Rv9)
     # extra recovered compartments so that waning immunity is not exponential
-    R_1w <- c(R_1w1, R_1w2, R_1w3, R_1w4, R_1w5, R_1w6, R_1w7, R_1w8, R_1w9)
-    Rv_1w <- c(Rv_1w1, Rv_1w2, Rv_1w3, Rv_1w4, Rv_1w5, Rv_1w6, Rv_1w7, Rv_1w8, Rv_1w9)
-
-    R_2w <- c(R_2w1, R_2w2, R_2w3, R_2w4, R_2w5, R_2w6, R_2w7, R_2w8, R_2w9)
-    Rv_2w <- c(Rv_2w1, Rv_2w2, Rv_2w3, Rv_2w4, Rv_2w5, Rv_2w6, Rv_2w7, Rv_2w8, Rv_2w9)
-
-    R_3w <- c(R_3w1, R_3w2, R_3w3, R_3w4, R_3w5, R_3w6, R_3w7, R_3w8, R_3w9)
-    Rv_3w <- c(Rv_3w1, Rv_3w2, Rv_3w3, Rv_3w4, Rv_3w5, Rv_3w6, Rv_3w7, Rv_3w8, Rv_3w9)
+    # R_1w <- c(R_1w1, R_1w2, R_1w3, R_1w4, R_1w5, R_1w6, R_1w7, R_1w8, R_1w9)
+    # Rv_1w <- c(Rv_1w1, Rv_1w2, Rv_1w3, Rv_1w4, Rv_1w5, Rv_1w6, Rv_1w7, Rv_1w8, Rv_1w9)
+    # 
+    # R_2w <- c(R_2w1, R_2w2, R_2w3, R_2w4, R_2w5, R_2w6, R_2w7, R_2w8, R_2w9)
+    # Rv_2w <- c(Rv_2w1, Rv_2w2, Rv_2w3, Rv_2w4, Rv_2w5, Rv_2w6, Rv_2w7, Rv_2w8, Rv_2w9)
+    # 
+    # R_3w <- c(R_3w1, R_3w2, R_3w3, R_3w4, R_3w5, R_3w6, R_3w7, R_3w8, R_3w9)
+    # Rv_3w <- c(Rv_3w1, Rv_3w2, Rv_3w3, Rv_3w4, Rv_3w5, Rv_3w6, Rv_3w7, Rv_3w8, Rv_3w9)
     # ---------------------------------------------------------------
     
     # vaccination rate ----------------------------------------------
-    if(is.null(t_vac_start) | is.null(t_vac_end)){ 
+    if(is.null(t_vac_start) | is.null(t_vac_end)){
       alpha <- c(rep(0,9))
-    } else if (!is.null(t_vac_start) & t >= t_vac_start & 
-               !is.null(t_vac_end) & t <= t_vac_end){
-      alpha <- vac_cov/(t_vac_end - t_vac_start + 1)
+    } else if (!is.null(t_vac_start) & times >= t_vac_start &
+               !is.null(t_vac_end) & times <= t_vac_end){
+      alpha <- alpha
     } else {alpha <- c(rep(0,9))}
+    
     # ---------------------------------------------------------------
     
     # determine contact matrix based on IC admissions ---------------
-    ic_admin <- sum(i1 * (H + Hv))
-    
+    ic_admissions <- sum(i1 * (H + Hv))
+
     # initialise flags
     if(times == 0 | params$keep_cm_fixed){
-      flag_relaxed <- 0
-      flag_very_relaxed <- 0
-      flag_normal <- 0
+      flag_open <- 0
     }
-    
+
     # determine contact matrix to use based on criteria
-    tmp2 <- choose_contact_matrix(params = params, 
-                                  criteria = ic_admisions, 
-                                  flag_relaxed = flag_relaxed, 
-                                  flag_very_relaxed = flag_very_relaxed, 
-                                  flag_normal = flag_normal, 
-                                  keep_fixed = keep_cm_fixed)
+    tmp2 <- choose_contact_matrix_simple(params = params,
+                                         criteria = ic_admissions,
+                                         flag_open = flag_open,
+                                         keep_fixed = params$keep_cm_fixed)
     contact_mat <- tmp2$contact_matrix
-    flag_relaxed <- tmp2$flag_relaxed
-    flag_very_relaxed <- tmp2$flag_very_relaxed
-    flag_normal <- tmp2$flag_normal
-    
+    flag_open <- tmp2$flag_open
+
     # determine force of infection ----------------------------------
     # seasonality
     calendar_day <- lubridate::yday(as.Date(times, origin = calendar_start_date))
@@ -87,53 +82,53 @@ age_struct_seir_simple <- function(times, init, params) {
     # calculate force of infection (lambda)
     lambda <- beta_t * (contact_mat %*% (I + (eta_trans * Iv)))
     # ---------------------------------------------------------------
-
+    
+    # vaccination denominator
+    denom <- S + R #+ R_1w + R_2w + R_3w
     #################################################################
     # ODEs:
-    dS  <- -lambda * S - alpha * S + (omega*4) * R_3w
-    dSv <- alpha * S - eta * lambda * Sv + (omega*4) * Rv_3w
+    dS  <- (-lambda * S) - (alpha * (S/denom)) + (omega * R)
+    dSv <- (alpha * (S/denom)) - (eta * lambda * Sv) + (omega * Rv)
 
-    dE  <- lambda * S - sigma * E + epsilon
-    dEv <- eta * lambda * Sv - sigma * Ev
+    dE  <- (lambda * S) - (sigma * E) + epsilon
+    dEv <- (eta * lambda * Sv) - (sigma * Ev)
 
-    dI  <- sigma * E - (gamma + h) * I
-    dIv <- sigma * Ev - (gamma + eta_hosp * h) * Iv
+    dI  <- (sigma * E)- (gamma * I) - (h * I)
+    dIv <- (sigma * Ev) - (gamma * Iv) - (eta_hosp * h * Iv)
 
-    dH  <- h * I - (i1 + d + r) * H
-    dHv <- eta_hosp * h * Iv - (i1 + d + r) * Hv
+    dH  <- (h * I) - (i1 * H) - (d * H) - (r * H)
+    dHv <- (eta_hosp * h * Iv) - (i1 * Hv) - (d * Hv) - (r * Hv)
 
-    dIC  <- i1 * H - (i2 + d_ic) * IC
-    dICv <- i1 * Hv - (i2 + d_ic) * ICv
+    dIC  <- (i1 * H) - (i2 * IC) - (d_ic * IC)
+    dICv <- (i1 * Hv) - (i2 * ICv) - (d_ic * ICv)
 
-    dH_IC  <- i2 * IC - (r_ic + d_hic) * H_IC
-    dH_ICv <- i2 * ICv - (r_ic + d_hic) * H_ICv
+    dH_IC  <- (i2 * IC) - (r_ic * H_IC) - (d_hic * H_IC)
+    dH_ICv <- (i2 * ICv) - (r_ic * H_ICv) - (d_hic * H_ICv)
 
-    dD <- d * (H + Hv) + d_ic * (IC + ICv) + d_hic * (H_IC + H_ICv)
+    dD <- (d * (H + Hv)) + (d_ic * (IC + ICv)) + (d_hic * (H_IC + H_ICv))
 
-    dR  <- (gamma * I) + (r * H) + (r_ic * H_IC) - ((omega*4) * R)
-    dRv <- (gamma * Iv) + (r * Hv) + (r_ic * H_ICv) - ((omega*4) * Rv)
+    dR  <- (gamma * I) + (r * H) + (r_ic * H_IC) - alpha * (R/denom) - (omega * R) 
+    dRv <- (gamma * Iv) + (r * Hv) + (r_ic * H_ICv) + alpha * (R/denom) - (omega * Rv)
 
-    dR_1w  <- (omega*4) * R - (omega*4) * R_1w
-    dRv_1w <- (omega*4) * Rv - (omega*4) * Rv_1w
-
-    dR_2w  <- (omega*4) * R_1w - (omega*4) * R_2w
-    dRv_2w <- (omega*4) * Rv_1w - (omega*4) * Rv_2w
-
-    dR_3w  <- (omega*4) * R_2w - (omega*4) * R_3w
-    dRv_3w <- (omega*4) * Rv_2w - (omega*4) * Rv_3w
+    # dR_1w  <- (omega*4) * R - alpha * (R_1w/denom) - (omega*4) * R_1w
+    # dRv_1w <- (omega*4) * Rv + alpha * (R_1w/denom) - (omega*4) * Rv_1w
+    # 
+    # dR_2w  <- (omega*4) * R_1w - alpha * (R_2w/denom) - (omega*4) * R_2w
+    # dRv_2w <- (omega*4) * Rv_1w + alpha * (R_2w/denom) - (omega*4) * Rv_2w
+    # 
+    # dR_3w  <- (omega*4) * R_2w - alpha * (R_3w/denom) - (omega*4) * R_3w
+    # dRv_3w <- (omega*4) * Rv_2w + alpha * (R_3w/denom) - (omega*4) * Rv_3w
     #################################################################
     dt <- 1
     
     # assign variables to global environment, so they can be used for
     # the next iteration
-    assign("flag_relaxed", flag_relaxed, envir = globalenv())
-    assign("flag_very_relaxed", flag_very_relaxed, envir = globalenv())
-    assign("flag_normal", flag_normal, envir = globalenv())
+    assign("flag_open", flag_open, envir = globalenv())
     
     # output --------------------------------------------------------
     list(c(dt, dS, dSv, dE, dEv, dI, dIv, dH, dHv, dIC, dIC,
-           dH_IC, dH_ICv, dD, dR, dRv, dR_1w, dRv_1w,
-           dR_2w, dRv_2w, dR_3w, dRv_3w
+           dH_IC, dH_ICv, dD, dR, dRv#, 
+           #dR_1w, dRv_1w,dR_2w, dRv_2w, dR_3w, dRv_3w
     ))
   })
 }
