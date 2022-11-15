586cbd33e2e26e1b83a38fa5c090fe61f02d3cc9
kylieainslie <ainslie.kylie@gmail.com>
2022-11-15 10:04:40 +0100

    add additional waves to visualise results script

--- inst/extdata/scripts/visualise_results.R
+++ inst/extdata/scripts/visualise_results.R
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Visualise scenario hub round 2 results
+# Visualise results
 # ------------------------------------------------------------------------------
 
 # load required packages -------------------------------------------------------
@@ -8,21 +8,37 @@ library(dplyr)
 library(ggplot2)
 
 # read in results df -----------------------------------------------------------
-df_wave1 <- readRDS("inst/extdata/results/covid_scenarios/scenario_results_wave1.rds")
+df_wave1 <- readRDS("inst/extdata/results/wave1_results.rds")
+df_wave2 <- readRDS("inst/extdata/results/wave2_results.rds")
+df_wave3 <- readRDS("inst/extdata/results/wave3_results.rds")
 
-# summarise over all age groups
-df_all <- df_wave1 %>%
-  #bind_rows(., df_round2_no_boost) %>%
-  group_by(scenario_id, target_variable, date, sample) %>%
-  summarise(sum = sum(value)) %>%
-  ungroup() %>%
+# create a single data frame
+df_all <- bind_rows(df_wave1, df_wave2, df_wave3, .id = "wave") %>%
   mutate(target_variable = factor(target_variable,
-                           levels = c("inc infection", "inc hosp", "inc icu", 
-                                      "inc death")),
-         sample = factor(sample))
+                                  levels = c("inc infection", "inc hosp", "occ hosp", 
+                                             "inc icu", "occ icu", "inc death")),
+         sample = factor(sample),
+         wave = factor(wave))
 
+# Line plot --------------------------------------------------------------------
+# plot each sample individually
+# first sum over age groups for each target_variable
+df_sum_ag <- df_all %>%
+  group_by(wave, scenario_id, target_variable, date, sample) %>%
+  summarise(sum = sum(value)) %>%
+  ungroup()
+
+# plot
+p_lines <- ggplot(data = df_sum_ag, aes(x = date, y = sum, group = sample, 
+                                     color = scenario_id)) +
+  geom_line() +
+  facet_gird(target_variable~wave)
+
+p_lines
+
+# Average with confidence bounds -----------------------------------------------
 df_summary <- df_all %>%
-  group_by(scenario_id, target_variable, date) %>%
+  group_by(wave,scenario_id, target_variable, date) %>%
   summarise(mean  = median(sum),
             q025 = quantile(sum, probs = 0.025),
             q25  = quantile(sum, probs = 0.25),
@@ -31,11 +47,11 @@ df_summary <- df_all %>%
             ) %>%
   select(date, scenario_id, target_variable, mean:q975) 
 
-# plot -------------------------------------------------------------------------
-# mean line with ribbon 
+# plot 
 p_ribbon <- ggplot(data = df_summary %>%
-                  filter(target_variable %in% c("inc infection")#,
-                         #date < as.Date("2022-10-01")
+                   filter(target_variable %in% c("occ hosp"),
+                          scenario_id != "A-Wave1"
+                  #        #date < as.Date("2022-10-01")
                          ), # "inc hosp", "inc icu", "inc death"
                  aes(x = date, y = mean, color = scenario_id, fill = scenario_id)) +
   geom_line() +
@@ -54,70 +70,15 @@ p_ribbon <- ggplot(data = df_summary %>%
         legend.box="vertical",
         axis.title=element_text(size=14,face="bold")) +
   guides(fill=guide_legend("Scenario ID"), colour = guide_legend("Scenario ID")) +
-  facet_grid(.~target_variable) +
+  facet_grid(.~target_variable, scales = "free") #+
   # annotate("rect", xmin = as.Date("2022-09-22"), xmax = as.Date("2022-12-15"), ymin = 0, ymax = 200000, 
   #          alpha = .5)
-  geom_vline(xintercept = as.Date("2022-09-15"), linetype = "dashed", color = "grey70")
+  #geom_vline(xintercept = as.Date("2022-09-15"), linetype = "dashed", color = "grey70")
 p_ribbon
 
+
+# save outputs -----------------------------------------------------------------
 ggsave(filename = "/rivm/s/ainsliek/results/scenario_hub/round2/case_plot_round2.jpg", 
        plot = p_ribbon,
        units = "in", height = 8, width = 13, dpi = 300)
 
-# individual lines
-p_lines <- ggplot(data = df_all %>%
-         filter(target_variable %in% c("inc infection") #,
-                #sample %in% sample(.data$sample, 9)
-                #scenario_id %in% c("A-2022-07-24", "B-2022-07-24")
-                ), #"inc hosp", "inc icu", "inc death"
-       aes(x = date, 
-           y = sum, 
-           group = sample, 
-           color = scenario_id)) +
-  geom_line() #+
-  #facet_wrap(~sample)
-p_lines
-
-  xlab("Date") + 
-  ylab("Mean value") +
-  scale_x_date(date_breaks = "1 month", date_labels = "%d %b %Y") +
-  theme(legend.position = "bottom",
-        panel.background = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1, size = 14),
-        axis.text.y = element_text(size = 14),
-        strip.text.x = element_text(size = 14),
-        legend.text = element_text(size = 14),
-        legend.title = element_text(size = 14),
-        legend.box="vertical",
-        axis.title=element_text(size=14,face="bold")) +
-  guides(fill=guide_legend("Scenario ID"), colour = guide_legend("Scenario ID")) +
-  facet_wrap(~target_variable, nrow = 1) 
-# annotate("rect", xmin = as.Date("2022-09-22"), xmax = as.Date("2022-12-15"), ymin = 0, ymax = 200000, 
-#          alpha = .5)
-p_lines +
-  geom_vline(xintercept = as.Date("2022-09-15"), linetype = "dashed", color = "grey70")
-
-# percent reduction ------------------------------------------------------------
-# total population
-# peak_inc <- df_all %>%
-#   group_by(scenario_id, target_variable, sample) %>%
-#   summarise(max = max(sum)) %>%
-#   ungroup() %>%
-#   group_by(scenario_id, target_variable) %>%
-#   summarise(max_mean = mean(max),
-#             max_q025 = quantile(max, probs = 0.025),
-#             max_q975 = quantile(max, probs = 0.975))
-# 
-# peak_incAB <- #peak_inc %>%
-#   df_all %>%
-#   group_by(scenario_id, target_variable, sample) %>%
-#   filter(scenario_id %in% c("A-2022-07-24", "B-2022-07-24")) %>%
-#   summarise(max = max(sum)) %>%
-#   mutate(perc_change = scales::percent((max - max[scenario_id == 'A-2022-07-24'])/max[scenario_id == 'A-2022-07-24']))
-# 
-# peak_incCD <- peak_inc %>%
-#   filter(scenario_id %in% c("C-2022-07-24", "D-2022-07-24")) %>%
-#   group_by(target_variable) %>%
-#   mutate(perc_change_mean = scales::percent((max_mean - max_mean[scenario_id == 'C-2022-07-24'])/max_mean[scenario_id == 'C-2022-07-24']),
-#          perc_change_q025 = scales::percent((max_q025 - max_q025[scenario_id == 'C-2022-07-24'])/max_q025[scenario_id == 'C-2022-07-24']),
-#          perc_change_q975 = scales::percent((max_q975 - max_q975[scenario_id == 'C-2022-07-24'])/max_q975[scenario_id == 'C-2022-07-24']))
